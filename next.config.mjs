db92dd44353a3a070d98afdde3f86797f548b294
Chojan Shang <psiace@apache.org>
2026-03-05 01:27:09 +0800

    fix: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+import fs from "node:fs"
+
 const isGitHubActions = process.env.GITHUB_ACTIONS === "true"
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""
 const isUserSite = repo.endsWith(".github.io")
-const basePath = isGitHubActions && !isUserSite ? `/${repo}` : ""
+const hasCustomDomain = fs.existsSync("public/CNAME")
+const useBasePath = isGitHubActions && !isUserSite && !hasCustomDomain
+const basePath = useBasePath ? `/${repo}` : ""
 
 const nextConfig = {
   output: "export",
